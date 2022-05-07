3c2312d3ade5b65876beb2c5000efd17553526e6
albertoornelasdev <albertoornelasdev@gmail.com>
2022-05-06 17:11:20 -0700

    Staircase reversed

--- src/main/java/Staircase.java
+++ src/main/java/Staircase.java
@@ -1,15 +1,28 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+
 
 public class Staircase {
+
+    //reverse
+    public static void staircase2(int n) {
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void staircase(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < n - i; j++) {
+                //System.out.printf("i:%s j:%s: \n", i , j);
                 System.out.print(" ");
             }
             for (int k = 1; k <= i; k++) {
+                //System.out.printf("i:%s k:%s: \n", i , k);
                 System.out.print("#");
             }
             System.out.println();
@@ -18,5 +31,6 @@ public class Staircase {
 
     public static void main(String[] args) {
         staircase(4);
+        staircase2(4);
     }
 }
